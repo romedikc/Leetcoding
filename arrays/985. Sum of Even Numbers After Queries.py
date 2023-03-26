5d7a62da177eb9a46792377b6af72555cce503b7
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-03-26 23:22:11 +0600

    26 mar

---
+++ arrays/985. Sum of Even Numbers After Queries.py
@@ -0,0 +1,17 @@
+def sumEvenAfterQueries(nums: List[int], queries: List[List[int]]) -> List[int]:
+    even_sum = sum(n for n in nums if n % 2 == 0)
+    output = []
+    for v, i in queries:
+        if nums[i] % 2 == 0:
+            even_sum -= nums[i]
+        nums[i] += v
+        # checks if a new added value is even
+        if nums[i] % 2 == 0:
+            even_sum += nums[i]
+        output.append(even_sum)
+    return output
+
+
+nums = [1, 2, 3, 4]
+queries = [[1, 0], [-3, 1], [-4, 0], [2, 3]]
+print(sumEvenAfterQueries(nums, queries))
