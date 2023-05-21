92d0db8a6a87281ca1c9a0aa6d7e2bc24ab73e3c
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-05-21 20:16:03 +0600

    21 may

---
+++ arrays/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,10 @@
+def maxProfit(prices: List[int]) -> int:
+    max_profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            max_profit += prices[i] - prices[i - 1]
+    return max_profit
+
+
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit(prices))
