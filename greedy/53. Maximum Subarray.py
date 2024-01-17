7660a1138a015a25d4cc4d0fae78609ac6ce53d2
romedikc <1ayanabdrahmanova1@gmail.com>
2024-01-17 16:19:42 +0600

    17 Jan

---
+++ greedy/53. Maximum Subarray.py
@@ -0,0 +1,11 @@
+def maxSubArray(nums: List[int]) -> int:
+    curr_sum = nums[0]
+    max_sum = nums[0]
+    for i in range(1, len(nums)):
+        curr_sum = max(nums[i], curr_sum + nums[i])
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray(nums))
